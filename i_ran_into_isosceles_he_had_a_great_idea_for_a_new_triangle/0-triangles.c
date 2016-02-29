c51696c9490f154477cd5975f9d2594f9822362d
jdeepee <joshuadparkin@gmail.com>
2016-02-28 22:03:44 -0800

    triangle

--- i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
+++ i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
@@ -1,3 +1,25 @@
-int print_char(char c);
+#include <stdio.h>
 
-void triangles_in_the_term(int h, int n)
+void triangles_in_the_term(int h, int n) {
+	if (n > 0 && h > 0) {
+		int s, m, v, i;
+		int t = 1;
+		for (m = h; m > 1; m--){ 
+				t = (t + 2); 
+		}
+		for (s = n; s > 0; s--){ 
+			int c = 1;
+			for (m = h; m > 0; m--){ 
+				v = (t - c)/2; 
+				for (i = v; i > 0; i--) {
+					printf(" "); 
+				}
+				for (i = c; i > 0; i--) {
+					printf("*"); 
+				}
+				c = (c + 2);
+				printf("\n");
+			}
+		}
+	}
+}
