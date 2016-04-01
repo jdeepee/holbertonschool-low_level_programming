579f2bfcccc517b203f547fc519c4199102973ac
jdeepee <joshuadparkin@gmail.com>
2016-03-31 22:11:36 -0700

    Fix

--- without_libraries_what_have_we_we_have_no_past_and_no_future/reverse_string.c
+++ without_libraries_what_have_we_we_have_no_past_and_no_future/reverse_string.c
@@ -1,17 +1,2 @@
 void reverse_string(char *s){
-	char c;
-	int length=0;
-	int loop=0;
-
-	while (s[loop] != '\0'){
-		length++;
-		loop++;
-	}
-
-	for(; left<right ; left++, right--)
-	{
-		c = *left;
-		*left = *right;
-		*right = c;
-	}
 }
